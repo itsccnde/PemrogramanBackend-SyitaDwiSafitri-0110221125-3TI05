efa5e91dadc817e8538d705bcfe345e622bb6ff0
itsccnde <92700590+itsccnde@users.noreply.github.com>
2022-12-25 15:05:41 +0700

    Add files via upload

---
+++ praktimu12/routes/api.js
@@ -0,0 +1,17 @@
+// Import Student Controller
+const StudentController = require("../controllers/StudentController");
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+router.get("/student", StudentController.index);
+router.post("/student", StudentController.store);
+// router.put("/student/:id", StudentController.update);
+// router.delete("/student/:id", StudentController.destroy);
+
+// Export router
+module.exports = router;
